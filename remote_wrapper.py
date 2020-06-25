337ed7ad7eb6d1e5851678a3802763d329913823
Mike Gering <mike@mfg-consulting-llc.com>
2020-06-25 14:26:22 -0400

    remote wrapper

---
+++ remote_wrapper.py
@@ -0,0 +1,14 @@
+import os
+import ptvsd
+import socket
+ptvsd.enable_attach(address = ('0.0.0.0', 8567))
+ 
+# Enable the line of source code below only if you want the application to wait until the debugger has attached to it
+#ptvsd.wait_for_attach()
+ 
+ptvsd.break_into_debugger()
+ 
+cwd = os.getcwd()
+ 
+print("Hello world you are here %s" % cwd )
+print("On machine %s" % socket.gethostname())
