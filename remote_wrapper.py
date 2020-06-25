0b6c2527e8f54dc4802598eebbb7499524b2d5b6
Mike Gering <mike@mfg-consulting-llc.com>
2020-06-25 14:39:38 -0400

    wrapper

--- remote_wrapper.py
+++ remote_wrapper.py
@@ -1,6 +1,7 @@
 import os
 import ptvsd
 import socket
+import transvpnmon
 ptvsd.enable_attach(address = ('0.0.0.0', 8567))
  
 # Enable the line of source code below only if you want the application to wait until the debugger has attached to it
@@ -13,3 +14,5 @@ cwd = os.getcwd()
  
 print("Hello world you are here %s" % cwd )
 print("On machine %s" % socket.gethostname())
+
+transvpnmon.run()
