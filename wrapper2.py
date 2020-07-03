ae186ca28d1ea4e5b312e7b2b8e2efe974079396
Mike Gering <mike@mfg-consulting-llc.com>
2020-07-03 13:48:23 -0400

    suff

--- wrapper2.py
+++ wrapper2.py
@@ -1,8 +1,10 @@
 import debugpy
+import os
 
 # 5678 is the default attach port in the VS Code debug configurations. Unless a host and port are specified, host defaults to 127.0.0.1
-debugpy.listen(5678)
+debugpy.listen(('192.168.1.13', 5678))
 print("Waiting for debugger attach")
 debugpy.wait_for_client()
 debugpy.breakpoint()
+print(os.system("ls /"))
 print('break on this line')
