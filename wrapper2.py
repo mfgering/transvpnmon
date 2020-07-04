cd1681420e0db51c2db9c94191bdae82aab42c34
Mike Gering <mike@mfg-consulting-llc.com>
2020-07-04 09:52:02 -0400

    progress

--- wrapper2.py
+++ wrapper2.py
@@ -1,10 +1,11 @@
 import debugpy
 import os
+import transvpnmon
 
 # 5678 is the default attach port in the VS Code debug configurations. Unless a host and port are specified, host defaults to 127.0.0.1
-debugpy.listen(('192.168.1.13', 5678))
+debugpy.listen(('192.168.1.13', 8567))
 print("Waiting for debugger attach")
 debugpy.wait_for_client()
-debugpy.breakpoint()
-print(os.system("ls /"))
-print('break on this line')
+#debugpy.breakpoint()
+transvpnmon.test()
+print("Done")
