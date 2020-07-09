22995a6683c6a0ce190e94e4d63d15e385d21ebd
Mike Gering <mike@mfg-consulting-llc.com>
2020-07-09 07:27:05 -0400

    test

--- test/ptest.py
+++ test/ptest.py
@@ -2,11 +2,19 @@
 import subprocess
 import pprint
 
+def service_action(service_name, action, timeout=10):
+	out_str = ""
+	err_str = ""
+	cmd = ["service", service_name, action]
+	p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	try:
+		stdout, stderr = p.communicate(timeout=8)
+	except subprocess.TimeoutExpired:
+		pass
+	stdout = stdout.decode('utf-8')
+	stderr = stderr.decode('utf-8')
+	return stdout.decode('utf-8'), 
 service_name = "3proxy"
-cmd = ["service", service_name, "start"]
-try:
-p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-stdout, stderr = p.communicate(timeout=8)
-print("out: {}".format(stdout.decode('utf-8')))
+print("out: {}".format())
 print("err: {}".format(stderr.decode('utf-8')))
 print(f"rc: {p.returncode}")
